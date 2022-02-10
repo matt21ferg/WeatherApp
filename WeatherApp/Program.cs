ed6f923e72256bd13b06e07f78c286fce794fd98
Matthew Ferguson <you@example.com>
2022-02-09 20:05:27 -0600

    done

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("AppSettingsRepo.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+                       
+
+            Console.WriteLine("what is your zip code?\n");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"http://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+            Console.WriteLine($"it is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location");
+            
+            
+        
+        
+
+
+        
+            
+        
+        }
+    }
+}
---
+++ WeatherApp/WeatherMap.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WeatherApp
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetAsync(apiCall).Result.ToString();
+
+           var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+           
+            return temp;
+        }
+
+    }
+}
